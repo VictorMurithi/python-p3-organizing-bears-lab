8fa7f224315a60f7b6d50934ef53620bfe29635f
victor <victor.murithi@student.moringaschool.com>
2023-11-30 12:28:51 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,21 @@
+INSERT INTO bears (name, sex) VALUES ('Mr. Chocolate', 'M');
+
+-- Inserting Rowdy
+INSERT INTO bears (name, sex) VALUES ('Rowdy', 'M');
+
+-- Inserting Tabitha
+INSERT INTO bears (name, sex) VALUES ('Tabitha', 'F');
+
+-- Inserting Sergeant Brown
+INSERT INTO bears (name, sex) VALUES ('Sergeant Brown', 'M');
+
+-- Inserting Melissa
+INSERT INTO bears (name, sex) VALUES ('Melissa', 'F');
+
+-- Inserting Grinch
+INSERT INTO bears (name, sex) VALUES ('Grinch', 'M');
+
+-- Inserting Wendy
+INSERT INTO bears (name, sex) VALUES ('Wendy', 'F');
+
+INSERT INTO bears (sex) VALUES ('F');
